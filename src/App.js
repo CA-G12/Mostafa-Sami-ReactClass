ef842d0fd3b4be286299c8b3ca545535a3681165
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-09-21 22:59:11 +0200

    handle components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import Cover from './components/Cover';
+import Nav from './components/Nav';
+import FilmInfo from './components/FilmInfo';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <></>
+    <>
+    <Nav />
+    <Cover />
+    <SearchBar />
+    <FilmInfo name ='Fargo' />
+    </>
   );
 }
 
