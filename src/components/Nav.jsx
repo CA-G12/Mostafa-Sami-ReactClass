954481981562cf6b449e2e30f4b93db9d0c5ff2a
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-09-21 22:16:38 +0200

    create navbar components relate # 4

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+class  Nav  extends React.Component {
+render() {
+
+  return (
+    <header>
+      <nav>
+        <h1>Movies Land</h1>
+        <ul>
+          <li>Home</li>
+          <li>List anime</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+}
+
+
+export default Nav;
