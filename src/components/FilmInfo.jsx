28d451aaa7c9933dcad96c85ac97340571373db2
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-09-22 10:05:52 +0200

    create film information component and display info in card relates #9

---
+++ src/components/FilmInfo.jsx
@@ -0,0 +1,49 @@
+import React /*, { useEffect } */ from "react";
+class  FilmInfo  extends React.Component {
+state = {
+    data: null,
+}
+    componentDidMount() {
+        fetch(`https://api.tvmaze.com/singlesearch/shows?q=fargo`)
+        .then((res) => res.json())
+        .then((data)=> this.setState({ data }))
+    }
+
+    componentDidUpdate = (prevProps)=>{
+        if( prevProps.movie?.id !== this.props.movie?.id ){
+            fetch(`https://api.tvmaze.com/shows/${this.props.movie.id}`)
+            .then((res) => res.json())
+            .then((data)=> this.setState({ data }))
+        }
+    }
+
+    
+    createMarkup(value) {
+        return {__html: `summary: ${value}`};
+    }
+
+
+render() {
+    if(!this.state.data ){
+        return <h2>loading ...</h2>
+    }
+
+
+    return (
+    <section id="details">
+    <div><img className="picture" src={this.state.data.image?.original || 'https://th.bing.com/th/id/OIP.BktOiHnYJ46AB8NzyeO6bgHaE8?pid=ImgDet&rs=1'  } alt="s" /></div>
+    <div>
+        <p className="name">suggestion: {this.state.data.name}</p>
+        <p className="rating">rating: {this.state.data.rating.average}</p>
+        <p className="summary"  dangerouslySetInnerHTML={this.createMarkup(this.state.data.summary)} >
+        {/* summary: {this.state.data.summary}  */}
+        </p>
+    </div>
+  </section>
+  );
+};
+
+}
+
+
+export default FilmInfo;
